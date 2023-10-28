05c4edc77f8e8adad21ea9ff7e7f978d8dfe9869
Lucino772 <lucapalmi772@gmail.com>
2023-10-28 16:01:34 +0200

    refactor: change minmax implementation (not all the tests pass)

--- project2-adversarial/src/adversarial_search.py
+++ project2-adversarial/src/adversarial_search.py
@@ -3,36 +3,31 @@ from mdp import MDP, S, A
 
 class _MinMaxAlgo:
     def _min(self, mdp: MDP[A, S], state: S, max_depth: int) -> float:
-        if mdp.is_final(state) or max_depth == 0:
-            return state.value
-
         best_value = float("+inf")
-
-        next_states = [state]
-        while len(next_states) > 0:
-            current_state = next_states.pop()
-            if current_state.current_agent == 0:
-                best_value = min(best_value, self._max(mdp, current_state, max_depth-1))
-            else:
-                next_states += [
-                    mdp.transition(current_state, action)
-                    for action in mdp.available_actions(current_state)
-                ]
+        for action in mdp.available_actions(state):
+            next_state = mdp.transition(state, action)
+            best_value = min(best_value, self.run(mdp, next_state, max_depth-1))
 
         return best_value
 
     def _max(self, mdp: MDP[A, S], state: S, max_depth: int) -> float:
-        if mdp.is_final(state) or max_depth == 0:
-            return state.value
-
         best_value = float("-inf")
         for action in mdp.available_actions(state):
             next_state = mdp.transition(state, action)
-            best_value = max(best_value, self._min(mdp, next_state, max_depth-1))
+            best_value = max(best_value, self.run(mdp, next_state, max_depth-1))
 
         return best_value
 
-    def minimax(self, mdp: MDP[A, S], state: S, max_depth: int) -> A:
+    def run(self, mdp: MDP[A, S], state: S, max_depth) -> float:
+        if mdp.is_final(state) or max_depth == 0:
+            return state.value
+
+        if state.current_agent == 0:
+            return self._max(mdp, state, max_depth)
+        else:
+            return self._min(mdp, state, max_depth)
+
+    def execute(self, mdp: MDP[A, S], state: S, max_depth: int) -> A:
         if state.current_agent != 0:
             raise ValueError
 
@@ -40,15 +35,16 @@ class _MinMaxAlgo:
         best_value = float("-inf")
         for action in mdp.available_actions(state):
             next_state = mdp.transition(state, action)
-            value = self._min(mdp, next_state, max_depth-1)
+            value = self.run(mdp, next_state, max_depth)
 
-            if value > best_value:
+            if value >= best_value:
                 best_action = action
                 best_value = value
 
         return best_action
 
-minimax = _MinMaxAlgo().minimax
+minimax = _MinMaxAlgo().execute
+
 
 def alpha_beta(mdp: MDP[A, S], state: S, max_depth: int) -> A:
     ...
