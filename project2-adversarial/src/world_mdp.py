0c4e9b75f074e897adc26eaa8c1d1fa9824f75c4
Lucino772 <lucapalmi772@gmail.com>
2023-10-28 09:57:58 +0200

    fix: fixed the way state's value is computed

--- project2-adversarial/src/world_mdp.py
+++ project2-adversarial/src/world_mdp.py
@@ -7,6 +7,7 @@ from mdp import MDP, State
 @dataclass
 class WorldMDPState(State):
     world_state: WorldState
+    gems_collected: int
 
     @property
     def current_agent_pos(self):
@@ -16,7 +17,7 @@ class WorldMDPState(State):
 class WorldMDP(MDP[Action, WorldMDPState]):
     def __init__(self, world: World):
         self.world = world
-        self.state = WorldMDPState(0, 0, self.world.get_state())
+        self.state = WorldMDPState(0, 0, self.world.get_state(), 0)
 
     def _actions(self, agent: int, action: Action):
         return [
@@ -43,38 +44,57 @@ class WorldMDP(MDP[Action, WorldMDPState]):
         current_world_state = self.world.get_state()
 
         if state.current_agent != 0:
-            return WorldMDPState(state.value, next_agent, current_world_state)
+            return WorldMDPState(state.value, next_agent, current_world_state, state.gems_collected)
 
         if step_reward == lle.REWARD_AGENT_DIED:
-            return WorldMDPState(lle.REWARD_AGENT_DIED, next_agent, current_world_state)
+            return WorldMDPState(lle.REWARD_AGENT_DIED, next_agent, current_world_state, state.gems_collected)
 
-        collected_gems_reward = self._collected_gems(state) * lle.REWARD_GEM_COLLECTED
+        # INFO: Only count gems collected by agent 0
+        collected_gems = state.gems_collected + self._collected_gems(state)
+        collected_gems_reward = collected_gems * lle.REWARD_GEM_COLLECTED
         just_exit_reward = self._agent_just_exit(state) * lle.REWARD_AGENT_JUST_ARRIVED
         return WorldMDPState(
-            collected_gems_reward+just_exit_reward,
+            state.value+collected_gems_reward+just_exit_reward,
             next_agent,
-            current_world_state
+            current_world_state,
+            collected_gems
         )
 
 
     def reset(self):
         self.n_expanded_states = 0
         self.world.reset()
-        self.state = WorldMDPState(0, 0, self.world.get_state())
+        self.state = WorldMDPState(0, 0, self.world.get_state(), 0)
+        return self.state
 
     def available_actions(self, state: WorldMDPState) -> list[Action]:
-        return self.world.available_actions()[state.current_agent]
+        current_state = self.world.get_state()
+        self.world.set_state(state.world_state)
+        actions = self.world.available_actions()[state.current_agent]
+        self.world.set_state(current_state)
+        return actions
 
     def is_final(self, state: WorldMDPState) -> bool:
         if state.value == lle.REWARD_AGENT_DIED:
             return True
 
-        return not any(state.world_state.gems_collected)
+        if not all(state.world_state.gems_collected):
+            return False
+
+        # We check if every agent is currently in one of the exit
+        exit_pos = self.world.exit_pos[:]
+        for agent_pos in state.world_state.agents_positions:
+            if agent_pos in exit_pos:
+                exit_pos.remove(agent_pos)
+
+        return len(exit_pos) == 0
 
     def transition(self, state: WorldMDPState, action: Action) -> WorldMDPState:
         self.n_expanded_states += 1
-        actions = self._actions(state.current_agent, action)
-        step_reward = self.world.step(actions)
+        self.world.set_state(state.world_state)
+        step_reward = self.world.step(
+            self._actions(state.current_agent, action)
+        )
         return self._get_next_state(state, step_reward)
 
 
