e9dbc328743854bff2bf52f4225f76749a884cdd
Lucino772 <lucapalmi772@gmail.com>
2023-12-17 16:23:53 +0100

    feat: found parameters for analisys

--- project4-rl/src/analysis.py
+++ project4-rl/src/analysis.py
@@ -12,20 +12,20 @@ class Parameters:
 
 
 def prefer_close_exit_following_the_cliff() -> Parameters:
-    return Parameters(...)
+    return Parameters(reward_live=0.8, gamma=0.1, noise=0)
 
 
 def prefer_close_exit_avoiding_the_cliff() -> Parameters:
-    return Parameters(...)
+    return Parameters(reward_live=0.8, gamma=0.1, noise=0.1)
 
 
 def prefer_far_exit_following_the_cliff() -> Parameters:
-    return Parameters(...)
+    return Parameters(reward_live=2, gamma=0.4, noise=0)
 
 
 def prefer_far_exit_avoiding_the_cliff() -> Parameters:
-    return Parameters(...)
+    return Parameters(reward_live=1, gamma=0.5, noise=0.5)
 
 
 def never_end_the_game() -> Parameters:
-    return Parameters(...)
+    return Parameters(reward_live=1, gamma=0, noise=0)
